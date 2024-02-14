1dd7569781ac3efd948adc3ac342196959d8df7c
PanMhue <157310362+PanMhue@users.noreply.github.com>
2024-02-15 00:57:01 +0630

    change admin psw

--- admin/login.php
+++ admin/login.php
@@ -2,7 +2,7 @@
   session_start();
   $name=$_POST['name'];
   $password=$_POST['password'];
-  if($name=="admin" and $password=="Libra23%"){
+  if($name=="admin" and $password=="admin12345"){
     $_SESSION['auth']=true;
     header("location:welcomeadmin.php");
   } else {
